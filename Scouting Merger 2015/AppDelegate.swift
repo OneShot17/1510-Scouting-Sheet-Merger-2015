fd92e17a8156900af286cbcbb5a8469f5741f847
Stephen Brimhall <stephen.brimhall@icloud.com>
2015-03-30 17:28:00 -0700

    Initial Commit

---
+++ Scouting Merger 2015/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Scouting Merger 2015
+//
+//  Created by Stephen Brimhall on 3/30/15.
+//  Copyright (c) 2015 Stephen Brimhall. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
